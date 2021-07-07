8f3b8ee23998177b9822f813509e6ea2a24075dc
casey <casey.khaled@gmail.com>
2021-07-07 16:16:29 +0700

    Week 1 day 1

---
+++ hands-on-1/script.js
@@ -0,0 +1,40 @@
+let tbl = document.createElement("table");
+let tblDiv = document.getElementById("tblDiv");
+let tblBody = document.createElement("tbody");
+tbl.style.border = '1px solid black';
+
+const students = [{
+        name: "Alice",
+        age: 20
+    },
+    {
+        name: "Bob",
+        age: 21
+    },
+    {
+        name: "Jane",
+        age: 20
+    }
+];
+
+
+tableCreate = () => {
+    for (let i = 0; i < students.length; i++) {
+        let row = document.createElement("tr");
+        let cell = document.createElement("td");
+        let cellText = document.createTextNode(
+            students[i].name + " - " + students[i].age
+        );
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        tblBody.appendChild(row);
+    }
+    tbl.appendChild(tblBody);
+    tblDiv.appendChild(tbl);
+}
+
+calculateAverage = () => {
+    const total = students.reduce((acc, students) => acc + students.age, 0);
+        let averageAge = document.getElementById("averageAge");
+        averageAge.innerHTML = total / students.length;
+}
---
+++ homework-1/script.js
@@ -0,0 +1,7 @@
+let titleValue = document.getElementById("title");
+let descValue = document.getElementById("desc");
+
+showValue = () => {
+    alert("Title: " + titleValue.value + '\n' +
+        "Description: " + descValue.value)
+}
